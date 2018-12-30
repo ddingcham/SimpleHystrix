c371096c47ee79ea313cde9f2666363e94875e89
ddingcham <qo0op308@gmail.com>
2018-12-30 22:31:40 +0900

    SimpleCommandTest.java:15
    com.netflix.hystrix.exception.HystrixRuntimeException: SimpleCommand
    command executed multiple times - this is not permitted.

--- src/test/java/com/ddingcham/example/hystrix/netflix/SimpleCommandTest.java
+++ src/test/java/com/ddingcham/example/hystrix/netflix/SimpleCommandTest.java
@@ -2,14 +2,23 @@ package com.ddingcham.example.hystrix.netflix;
 
 import org.junit.Test;
 
+import java.util.concurrent.Future;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SimpleCommandTest {
 
     @Test
-    public void testSynchronous() {
-        assertThat(new SimpleCommand("sync").execute()).contains("sync");
+    public void execute() throws Exception {
+        SimpleCommand command = new SimpleCommand("execute");
+        assertThat(command.execute()).contains("execute");
+        assertThat(command.execute()).isEqualTo(command.queue().get());
     }
 
-
+    @Test
+    public void queue() throws Exception {
+        assertThat(new SimpleCommand("async").queue());
+        Future<String> future = new SimpleCommand("async").queue();
+        assertThat(future.get());
+    }
 }
