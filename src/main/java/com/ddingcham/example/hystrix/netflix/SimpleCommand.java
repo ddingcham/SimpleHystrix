460c0f78730617f637478625cf8a67b80effbd26
ddingcham <qo0op308@gmail.com>
2019-01-01 21:22:13 +0900

    SimpleCommand link // see also [SemaphoreIsolation], [Thread-Pool]

--- src/main/java/com/ddingcham/example/hystrix/netflix/SimpleCommand.java
+++ src/main/java/com/ddingcham/example/hystrix/netflix/SimpleCommand.java
@@ -3,6 +3,9 @@ package com.ddingcham.example.hystrix.netflix;
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 
+// see also
+// https://github.com/Netflix/Hystrix/blob/master/hystrix-examples/src/main/java/com/netflix/hystrix/examples/basic/CommandUsingSemaphoreIsolation.java
+// https://github.com/Netflix/Hystrix/wiki/How-To-Use#command-thread-pool
 public class SimpleCommand extends HystrixCommand<String> {
     static final String COMMAND_ID = "SimpleCommand";
     private final String name;
